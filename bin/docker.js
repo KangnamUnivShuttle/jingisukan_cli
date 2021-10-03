c0bf64fd974544fe83cd12923f4a978575512ab7
Kim HyunWoo <stories2@naver.com>
2021-10-03 23:56:40 +0900

    Update docker.js

--- bin/docker.js
+++ bin/docker.js
@@ -1,4 +1,5 @@
 import yaml from 'write-yaml'
+import { exec } from 'child_process'
 
 export function exportDockerComposeYAML(options) {
     const {
@@ -9,6 +10,10 @@ export function exportDockerComposeYAML(options) {
         path = '.'
     } = options
 
+    if (!name.match(/^[a-zA-Z]{1}[a-zA-Z0-9_.-]{7,15}$/g)) {
+        throw new Error('Unavailable letter inside of name')
+    }
+
     const data = {
         version: '3',
         networks: {
@@ -18,7 +23,8 @@ export function exportDockerComposeYAML(options) {
         },
         services: {
             plugin_node: {
-                image: '',
+                image: `${name}`,
+                build: `${path}/${name}/`,
                 container_name: name,
                 restart: 'always',
                 networks: ['chatbot'],
@@ -34,11 +40,92 @@ export function exportDockerComposeYAML(options) {
             }
         }
     }
-    yaml(`${path}/docker-compose.yaml`, data, (err) => {
+    yaml(`${path}/${name}/docker-compose.yaml`, data, (err) => {
         if (err) {
             console.error(`[docker] [exportDockerComposeYAML] error: ${err.message}`)
             return;
         }
         console.log('[docker] [exportDockerComposeYAML] done');
     });      
+}
+
+export function runDockerCompose(options) {
+    const {
+        status = 'undefined',
+        name = 'TEST',
+        path = '.'
+    } = options
+    let command = '';
+    switch(status) {
+        case 'start':
+            command = `docker-compose up -d --file=${path}/${name}/docker-compose.yaml`
+            break;
+        case 'stop':
+            command = `docker-compose stop --file=${path}/${name}/docker-compose.yaml`
+            break;
+        case 'remove':
+            command = `docker-compose rm -f --file=${path}/${name}/docker-compose.yaml`
+            break;
+        default:
+            throw new Error(`Unexpected status detected, ${status}!=[start|stop|remove]`)
+    }
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+            return;
+        }
+        console.log(`${stdout}`);
+    });
+}
+
+export function checkDockerContainerStatus (name) {
+    const command = `docker ps --filter "name=${name}"`
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+            return;
+        }
+        console.log(`${stdout}`);
+    });
+}
+
+export function runPrune () {
+    const command = `docker system prune -f`
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+            return;
+        }
+        console.log(`${stdout}`);
+    });
+}
+
+export function removeImage (name) {
+    if (!name.match(/^[a-zA-Z]{1}[a-zA-Z0-9_.-]{7,15}$/g)) {
+        throw new Error('Unavailable letter inside of name')
+    }
+    const command = `docker rmi ${name}:latest -f`
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`${stderr}`);
+            return;
+        }
+        console.log(`${stdout}`);
+    });
 }
